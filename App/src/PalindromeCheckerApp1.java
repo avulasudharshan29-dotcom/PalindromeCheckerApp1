fe08b0debe01a6e423b50cc29aeb699bffa2a904
https://github.com/avulasudharshan29-dotcom <avulasudharshan29@gmail.com>
2026-02-25 11:03:26 +0530

    U9 completed

--- App/src/PalindromeCheckerApp1.java
+++ App/src/PalindromeCheckerApp1.java
@@ -1,49 +1,25 @@
-import java.util.Stack;
 public class PalindromeCheckerApp1 {
+
     public static void main(String[] args) {
-        String input = "noon";
-        Stack<Character> stack = new Stack<>();
-        for (char c : input.toCharArray()) {
-            stack.push(c);
-        }
-        boolean isPalindrome = true;
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                isPalindrome = false;
-                break;
-            }
-        }
+
+        String input = "madam";
+
+        boolean result = check(input, 0, input.length() - 1);
+
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
-// File: UseCase4PalindromeCheckerApp.java
-import java.util.Scanner;
-
-public class PalindromeCheckerApp1 {
-    public static boolean isPalindrome(String input) {
-        input = input.replaceAll("\\s+", "").toLowerCase();
-        char[] charArray = input.toCharArray();
-        int start = 0;
-        int end = charArray.length - 1;
-
-        while (start < end) {
-            if (charArray[start] != charArray[end]) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
+        System.out.println("Is Palindrome? : " + result);
     }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("=== UC4: Character Array Based Palindrome Checker ===");
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-        if (isPalindrome(input)) {
-            System.out.println("Result: \"" + input + "\" is a palindrome.");
-        } else {
-            System.out.println("Result: \"" + input + "\" is NOT a palindrome.");
+
+    private static boolean check(String s, int start, int end) {
+
+        if (start >= end) {
+            return true;
         }
-        scanner.close();
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        return check(s, start + 1, end - 1);
     }
 }
