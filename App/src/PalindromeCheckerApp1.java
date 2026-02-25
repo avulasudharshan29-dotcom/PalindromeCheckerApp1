0d103abfb189e4ee176d73dc038331f32a1114d4
https://github.com/avulasudharshan29-dotcom <avulasudharshan29@gmail.com>
2026-02-25 10:54:08 +0530

    U7 completed

--- App/src/PalindromeCheckerApp1.java
+++ App/src/PalindromeCheckerApp1.java
@@ -1,13 +1,20 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
+
 public class PalindromeCheckerApp1 {
+
     public static void main(String[] args) {
-        String input = "madam";
+
+        String input = "refer";
+
         Deque<Character> deque = new ArrayDeque<>();
+
         for (char c : input.toCharArray()) {
-            deque.addLast(c);
+            deque.add(c);
         }
+
         boolean isPalindrome = true;
+
         while (deque.size() > 1) {
             char first = deque.removeFirst();
             char last = deque.removeLast();
@@ -17,11 +24,8 @@ public class PalindromeCheckerApp1 {
                 break;
             }
         }
-        System.out.println("Welcome to the Palindrome Checker Application!");
-        if (isPalindrome) {
-            System.out.println(input + " is a Palindrome");
-        } else {
-            System.out.println(input + " is NOT a Palindrome");
-        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
