b18a69f4eeef2afcd761aa7ad2df03a2ad5aa8ac
https://github.com/avulasudharshan29-dotcom <avulasudharshan29@gmail.com>
2026-02-25 10:07:50 +0530

    UC3 completed

--- App/src/PalindromeCheckerApp1.java
+++ App/src/PalindromeCheckerApp1.java
@@ -1,16 +1,17 @@
 public class PalindromeCheckerApp1 {
-
     public static boolean isPalindrome(String text) {
-        String rev = "";
+        String reversed = "";
         for (int i = text.length() - 1; i >= 0; i--) {
-            rev += text.charAt(i);
+            reversed = reversed + text.charAt(i);
         }
-        return text.equalsIgnoreCase(rev);
+        return text.equals(reversed);
     }
 
     public static void main(String[] args) {
-        String input = "madam";
 
+        System.out.println("Welcome to the Palindrome Checker Application!");
+
+        String input = "madam";
         if (isPalindrome(input)) {
             System.out.println(input + " is a Palindrome");
         } else {
